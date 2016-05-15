8d5547383daaf16bb70150d8a60f415503eaa08b
speed2CZ <gtgtgt@seznam.cz>
2016-05-15 11:32:56 +0200

    First version of Theta Passage

---
+++ FAF_TUT_Theta_BO/FAF_TUT_Theta_BO_scenario.lua
@@ -0,0 +1,30 @@
+version = 3
+ScenarioInfo = {
+    name = 'Theta Passage - Build Order',
+    description = "Introduction of the map, showing important locations and initial build order.",
+    type = 'campaign_coop',
+    starts = true,
+    preview = '',
+    size = {256, 256},
+    map = '/maps/SCMP_012/SCMP_012.scmap',
+    map_version = 1,
+    save = '/maps/FAF_TUT_Theta_BO/FAF_TUT_Theta_BO_save.lua',
+    script = '/maps/FAF_TUT_Theta_BO/FAF_TUT_Theta_BO_script.lua',
+    norushradius = 0.000000,
+    norushoffsetX_Player = 0.000000,
+    norushoffsetY_Player = 0.000000,
+    norushoffsetX_speed2_AI = 0.000000,
+    norushoffsetY_speed2_AI = 0.000000,
+    norushoffsetX_ARMY_9 = 0.000000,
+    norushoffsetY_ARMY_9 = 0.000000,
+    norushoffsetX_NEUTRAL_CIVILIAN = 0.000000,
+    norushoffsetY_NEUTRAL_CIVILIAN = 0.000000,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                { name = 'FFA', armies = {'Player','speed2_AI','ARMY_9','NEUTRAL_CIVILIAN',} },
+            },
+            customprops = {
+            },
+        },
+    }}
---
+++ FAF_TUT_Theta_BO/FAF_TUT_Theta_BO_strings.lua
@@ -0,0 +1,44 @@
+Intro = {
+  {text = '[speed2]: Welcome to this Supreme Commander Forged Alliance Forever map tutorial. This tutorial will teach you about the classic 1v1 map Theta passage. Let\'s get started.', vid = '', bank = '', cue = '', faction = 'UEF'},
+  {text = '[speed2]: Theta is a 5x5 land map, the smallest basic size map in Forged Alliance. The most important strategy to learn for this map is T1 land spam which we will show you shortly. For now let\'s take a look at the key features of the map.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+StartPosition = {
+	{text = '[speed2]: You start in one corner of the map with 4 Mass Extractors close and a Hydrocarbon Power Plant. It is important to integrate the Hydrocarbon Power Plant into your opening build order because it is more efficient than T1 Power Gerenators.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+ReclaimNearBase = {
+	{text = '[speed2]: These three reclaim fields are important to boost your early land production.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+ReclaimMiddle = {
+	{text = '[speed2]: The rocks around the arch in mid are useful, it\'s even better if you can steal the rocks on your opponents side of the arch.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+MiddleWrecks = {
+	{text = '[speed2]: The wrecks above the arch however have very little mass in them, don\'t waste time trying to reclaim them.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+InfoAboutMiddle = {
+	{text = '[speed2]: Raids can often move through this passage, build a wall to block invaders or send your own units through to raid your opponent\'s Mass Extractors.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+BottomPassage = {
+	{text = '[speed2]: There are also rocks to reclaim in this area. A wall or point defence will stop intruders. Early raiding through this passage can deny several Mass Extractors, take care not to get raided from this position.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+NorthPart = {
+	{text = '[speed2]: The open area at the north is the most important area of the map, it will be where the largest fights will happen. Keep you commander in this area and support him with the majority of your army.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+NorthPlateau = {
+	{text = '[speed2]: The plateau above contains 2 Mass Extractors which are the most contested Mass Extractors. They are also the easiest to secure if you can grab them thanks to the small choke units will have to use to reach them.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+NorthPlateau2 = {
+	{text = '[speed2]: If you can secure these two Mass Extractors for example with a pd, you will have the map control advantage. Mantain this map control and you will be well on your way to winning the game.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+StartBuildOrder = {
+	{text = '[speed2]: Now it\'s time to show some build order.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
