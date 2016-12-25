a3eca982ab21bc644063d063330424e48824d676
speed2CZ <GTGTGT@seznam.cz>
2016-12-25 16:54:56 +0100

    Add Loki
    by Tokyto

---
+++ maps/FAF_TUT_Loki_BO/FAF_TUT_Loki_BO_scenario.lua
@@ -0,0 +1,25 @@
+version = 3
+ScenarioInfo = {
+    name = 'Loki - Build Order',
+    description = 'Loki is one of the first worlds colonyzed and is famous for its rustic citys and above all abundant natural resources. Because of Lokis proximity to Earth it was never a primary target or even threatend during the entire infinite war. Untill the Seraphim came. Thanks to Timmyfred, Battlemoose, Scyn, Brn4mplz and everyone else who helped playtesting. Thanks to Saya for scripting ultimate defense and zone control so I had code to steal.',
+    type = 'tutorial',
+    starts = true,
+    preview = '',
+    size = {512, 512},
+    map = '/maps/FAF_TUT_Loki_BO/FAF_TUT_Loki_BO.scmap',
+    save = '/maps/FAF_TUT_Loki_BO/FAF_TUT_Loki_BO_save.lua',
+    script = '/maps/FAF_TUT_Loki_BO/FAF_TUT_Loki_BO_script.lua',
+    norushradius = 0.000000,
+    norushoffsetX_ARMY_1 = 0.000000,
+    norushoffsetY_ARMY_1 = 0.000000,
+    norushoffsetX_ARMY_2 = 0.000000,
+    norushoffsetY_ARMY_2 = 0.000000,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                { name = 'FFA', armies = {'Player','Tokyto_AI','NEUTRAL_CIVILIAN',} },
+            },
+            customprops = {
+            },
+        },
+    }}
---
+++ maps/FAF_TUT_Loki_BO/FAF_TUT_Loki_BO_strings.lua
@@ -0,0 +1,102 @@
+Intro_Choice = {
+	{text = '[Tokyto]: Welcome commander, choose if you want to hear the basic information about the map, watch an example build order or try the build order yourself.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+
+
+---------------------
+-- Cinematic map info
+---------------------
+
+
+
+MapInfo = {
+  {text = '[Tokyto]: Welcome to this Supreme Commander Forged Alliance Forever map tutorial. This tutorial will teach you about the classic 1v1 map Theta passage. Let\'s get started.', vid = '', bank = '', cue = '', faction = 'UEF'},
+  {text = '[Tokyto]: Theta is a 5x5 land map, the smallest basic size map in Forged Alliance. The most important strategy to learn for this map is T1 land spam which we will show you shortly. For now let\'s take a look at the key features of the map.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+StartPosition = {
+	{text = '[Tokyto]: You start in one corner of the map with 4 Mass Extractors close and a Hydrocarbon Power Plant. It is important to integrate the Hydrocarbon Power Plant into your opening build order because it is more efficient than T1 Power Gerenators.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+ReclaimNearBase = {
+	{text = '[Tokyto]: These three reclaim fields are important to boost your early land production.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+ReclaimMiddle = {
+	{text = '[Tokyto]: The rocks around the arch in mid are useful, it\'s even better if you can steal the rocks on your opponents side of the arch.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+MiddleWrecks = {
+	{text = '[Tokyto]: The wrecks above the arch however have very little mass in them, don\'t waste time trying to reclaim them.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+InfoAboutMiddle = {
+	{text = '[Tokyto]: Raids can often move through this passage, build a wall to block invaders or send your own units through to raid your opponent\'s Mass Extractors.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+BottomPassage = {
+	{text = '[Tokyto]: There are also rocks to reclaim in this area. A wall or point defence will stop intruders. Early raiding through this passage can deny several Mass Extractors, take care not to get raided from this position.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+NorthPart = {
+	{text = '[Tokyto]: The open area at the north is the most important area of the map, it will be where the largest fights will happen. Keep you commander in this area and support him with the majority of your army.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+NorthPlateau = {
+	{text = '[Tokyto]: The plateau above contains 2 Mass Extractors which are the most contested Mass Extractors. They are also the easiest to secure if you can grab them thanks to the small choke units will have to use to reach them.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+NorthPlateau2 = {
+	{text = '[Tokyto]: If you can secure these two Mass Extractors for example with a Point Defense, you will have the map control advantage. Mantain this map control and you will be well on your way to winning the game.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+StartBuildOrder = {
+	{text = '[Tokyto]: Now it\'s time to show some build order.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+
+
+--------------
+-- Build order
+--------------
+
+
+
+StartBuildOrder = {
+	{text = '[Tokyto]: Hold down SHIFT key to see command queue of the units. You can also select an ACU or engineers to see their build queue.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+LandFirst = {
+	{text = '[Tokyto]: Land Factory is built first, followed by two Power Generators and then 4 Mass Extractors.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+FactoryQeue = {
+	{text = '[Tokyto]: Factory will build 3 engineers. First one for expanding East, Second one for expanding North and Third one for expanding West. Then 2 tanks and 2 scouts to protect your expanding engineers and/or for raiding enemy engineers.', vid = '', bank = '', cue = '', faction = 'UEF'},
+	{text = '[Tokyto]: After that you can build additional engineers and tanks. On a heavy expansion map like Theta you should not build only engineers from your fist factory early on, else you might find yourself lacking units to protect your existing engineers.', vid = '', bank = '', cue = '', faction = 'UEF'},
+	{text = '[Tokyto]: Changing factory rally point will also affect which way the units will leave the factory, you can save some time by that.', vid = '', bank = '', cue = '', faction = 'UEF'},
+	{text = '[Tokyto]: If one of your expanding engineers die, make sure to replace him as SOON as possible! Every second matters', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+FirstEngineer = {
+	{text = '[Tokyto]: First engineer will go to build two Mass Extractors and a Hydrocarbon Powerplant on East expansion. Then he will build a factory over the lake. This expansion might be protected by enemy raiding units, so building a radar and factory first is recommended.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+SecondEngineer = {
+	{text = '[Tokyto]: Second Engineer can expand to the North. This engineer is very vulnerable to raiding units, so an escort is highly recommended. It will get the three mexes in the expansion and then constructing a basic base.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+WestEngineer = {
+	{text = '[Tokyto]: Third engineer will expand to the West. This engineer is not as important as previous ones, since it\'s job can be done with ACU, heading the same way.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+SecondFactory = {
+	{text = '[Tokyto]: Your second Land Factory will produce air units to scout your opponent and protect you from any bomber raids', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+
+ThirdFactory = {
+	{text = '[Tokyto]: Your third factory will be a land factory, which will focus on producing offensive T1 Land units.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
+CommanderMiddle = {
+	{text = '[Tokyto]: From now on the game is in your hands. I recommended using your newly built T1 engineers to build more factories, which will assist your second Land Factory in producing units. You will also need to build some more Energy Generators for their upkeep.', vid = '', bank = '', cue = '', faction = 'UEF'},
+}
