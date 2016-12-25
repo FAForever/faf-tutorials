7649cbab1e4cc76e21b31c8a7768044c01308f17
speed2CZ <GTGTGT@seznam.cz>
2016-12-25 16:54:02 +0100

    Add init file

---
+++ init_tutorials.lua
@@ -0,0 +1,113 @@
+dofile(InitFileDir .. '\\..\\fa_path.lua')
+
+path = {}
+
+whitelist =
+{
+    "effects.nx2",
+    "env.nx2",
+    "etc.nx2",
+    "loc.nx2",
+    "lua.nx2",
+    "meshes.nx2",
+    "mods.nx2",
+    "modules.nx2",
+    "projectiles.nx2",
+    "schook.nx2",
+    "textures.nx2",
+    "units.nx2",
+    "murderparty.nxt",
+    "labwars.nxt",
+    "units.scd",
+    "textures.scd",
+    "skins.scd",
+    "schook.scd",
+    "props.scd",
+    "projectiles.scd",
+    "objects.scd",
+    "moholua.scd",
+    "mohodata.scd",
+    "mods.scd",
+    "meshes.scd",
+    "lua.scd",
+    "loc_us.scd",
+    "loc_es.scd",
+    "loc_fr.scd",
+    "loc_it.scd",
+    "loc_de.scd",
+    "loc_ru.scd",
+    "env.scd",
+    "effects.scd",
+    "editor.scd",
+    "ambience.scd",
+    "advanced strategic icons.nxt",
+    "lobbymanager.scd",
+    "texturepack.nxt",
+    "sc_music.scd"
+}
+
+local function mount_dir(dir, mountpoint)
+    table.insert(path, { dir = dir, mountpoint = mountpoint } )
+end
+
+
+local function mount_contents(dir, mountpoint)
+    LOG('checking ' .. dir)
+    for _,entry in io.dir(dir .. '\\*') do
+        if entry != '.' and entry != '..' then
+        local mp = string.lower(entry)
+        local safe = true
+        mp = string.gsub(mp, '[.]scd$', '')
+        mp = string.gsub(mp, '[.]zip$', '')
+        mount_dir(dir .. '\\' .. entry, mountpoint .. '/' .. mp)
+        end
+    end
+end
+
+local function mount_dir_with_whitelist(dir, glob, mountpoint)
+    sorted = {}
+    LOG('checking ' .. dir .. glob)
+    for _,entry in io.dir(dir .. glob) do
+        if entry != '.' and entry != '..' then
+            local mp = string.lower(entry)
+            local notsafe = true
+            for i, white in whitelist do
+                notsafe = notsafe and (string.find(mp, white, 1) == nil)
+            end
+            if notsafe then
+                LOG('not safe ' .. dir .. entry)
+            else
+                table.insert(sorted, dir .. entry)
+            end
+        end
+    end
+    table.sort(sorted)
+    table.foreach(sorted, function(k,v) mount_dir(v, mountpoint) end)
+end
+
+-- mount mods
+mount_contents(SHGetFolderPath('PERSONAL') .. 'My Games\\Gas Powered Games\\Supreme Commander Forged Alliance\\mods', '/mods')
+mount_contents(InitFileDir .. '\\..\\user\\My Games\\Gas Powered Games\\Supreme Commander Forged Alliance\\mods', '/mods')
+
+mount_dir(InitFileDir .. '\\..\\gamedata\\*.tut', '/')
+
+mount_dir_with_whitelist(InitFileDir .. '\\..\\gamedata\\', '*.nxt', '/')
+mount_dir_with_whitelist(InitFileDir .. '\\..\\gamedata\\', '*.nx2', '/')
+
+mount_dir_with_whitelist(fa_path .. '\\gamedata\\', '*.scd', '/')
+mount_dir(fa_path, '/')
+
+hook = {
+    '/schook',
+    '/mods/tutorials/hook'
+}
+
+protocols = {
+    'http',
+    'https',
+    'mailto',
+    'ventrilo',
+    'teamspeak',
+    'daap',
+    'im',
+}
