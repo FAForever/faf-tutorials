b0678bf285392a21b673b860553caaefda1e150b
speed2CZ <GTGTGT@seznam.cz>
2017-02-27 21:38:10 +0100

    Fix indentation

--- maps/FAF_TUT_Theta_BO/FAF_TUT_Theta_BO_strings.lua
+++ maps/FAF_TUT_Theta_BO/FAF_TUT_Theta_BO_strings.lua
@@ -1,5 +1,5 @@
 Intro_Choice = {
-	{text = '[speed2]: Welcome commander, choose if you want to hear the basic information about the map, watch an example build order or try the build order yourself.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Welcome commander, choose if you want to hear the basic information about the map, watch an example build order or try the build order yourself.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 
@@ -11,48 +11,48 @@ Intro_Choice = {
 
 
 MapInfo = {
-  {text = '[speed2]: Welcome to this Supreme Commander Forged Alliance Forever map tutorial. This tutorial will teach you about the classic 1v1 map Theta passage. Let\'s get started.', vid = '', bank = '', cue = '', faction = 'UEF'},
-  {text = '[speed2]: Theta is a 5x5 land map, the smallest basic size map in Forged Alliance. The most important strategy to learn for this map is T1 land spam which we will show you shortly. For now let\'s take a look at the key features of the map.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Welcome to this Supreme Commander Forged Alliance Forever map tutorial. This tutorial will teach you about the classic 1v1 map Theta passage. Let\'s get started.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Theta is a 5x5 land map, the smallest basic size map in Forged Alliance. The most important strategy to learn for this map is T1 land spam which we will show you shortly. For now let\'s take a look at the key features of the map.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 StartPosition = {
-	{text = '[speed2]: You start in one corner of the map with 4 Mass Extractors close and a Hydrocarbon Power Plant. It is important to integrate the Hydrocarbon Power Plant into your opening build order because it is more efficient than T1 Power Gerenators.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: You start in one corner of the map with 4 Mass Extractors close and a Hydrocarbon Power Plant. It is important to integrate the Hydrocarbon Power Plant into your opening build order because it is more efficient than T1 Power Gerenators.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 ReclaimNearBase = {
-	{text = '[speed2]: These three reclaim fields are important to boost your early land production.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: These three reclaim fields are important to boost your early land production.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 ReclaimMiddle = {
-	{text = '[speed2]: The rocks around the arch in mid are useful, it\'s even better if you can steal the rocks on your opponents side of the arch.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: The rocks around the arch in mid are useful, it\'s even better if you can steal the rocks on your opponents side of the arch.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 MiddleWrecks = {
-	{text = '[speed2]: The wrecks above the arch however have very little mass in them, don\'t waste time trying to reclaim them.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: The wrecks above the arch however have very little mass in them, don\'t waste time trying to reclaim them.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 InfoAboutMiddle = {
-	{text = '[speed2]: Raids can often move through this passage, build a wall to block invaders or send your own units through to raid your opponent\'s Mass Extractors.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Raids can often move through this passage, build a wall to block invaders or send your own units through to raid your opponent\'s Mass Extractors.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 BottomPassage = {
-	{text = '[speed2]: There are also rocks to reclaim in this area. A wall or point defence will stop intruders. Early raiding through this passage can deny several Mass Extractors, take care not to get raided from this position.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: There are also rocks to reclaim in this area. A wall or point defence will stop intruders. Early raiding through this passage can deny several Mass Extractors, take care not to get raided from this position.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 NorthPart = {
-	{text = '[speed2]: The open area at the north is the most important area of the map, it will be where the largest fights will happen. Keep you commander in this area and support him with the majority of your army.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: The open area at the north is the most important area of the map, it will be where the largest fights will happen. Keep you commander in this area and support him with the majority of your army.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 NorthPlateau = {
-	{text = '[speed2]: The plateau above contains 2 Mass Extractors which are the most contested Mass Extractors. They are also the easiest to secure if you can grab them thanks to the small choke units will have to use to reach them.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: The plateau above contains 2 Mass Extractors which are the most contested Mass Extractors. They are also the easiest to secure if you can grab them thanks to the small choke units will have to use to reach them.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 NorthPlateau2 = {
-	{text = '[speed2]: If you can secure these two Mass Extractors for example with a Point Defense, you will have the map control advantage. Mantain this map control and you will be well on your way to winning the game.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: If you can secure these two Mass Extractors for example with a Point Defense, you will have the map control advantage. Mantain this map control and you will be well on your way to winning the game.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 StartBuildOrder = {
-	{text = '[speed2]: Now it\'s time to show some build order.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Now it\'s time to show some build order.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 
@@ -64,39 +64,39 @@ StartBuildOrder = {
 
 
 StartBuildOrder = {
-	{text = '[speed2]: Hold down SHIFT key to see command qeue of the units. You can also select an ACU or engineers to see their build qeue.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Hold down SHIFT key to see command qeue of the units. You can also select an ACU or engineers to see their build qeue.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 LandFirst = {
-	{text = '[speed2]: Land Factory is built first, followed by a single Power Generator. Then 4 Mass Extractors along the way to the Hydrocarbon.', vid = '', bank = '', cue = '', faction = 'UEF'},
-	{text = '[speed2]: Be sure to place move order closer to the Mass Extractors to utilize full build range of your ACU. Build range is the small yellow circle.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Land Factory is built first, followed by a single Power Generator. Then 4 Mass Extractors along the way to the Hydrocarbon.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Be sure to place move order closer to the Mass Extractors to utilize full build range of your ACU. Build range is the small yellow circle.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 FactoryQeue = {
-	{text = '[speed2]: Factory will build 3 engineers. One for building your base, 2 for expanding. Then 2 tanks and 2 scouts to protect your expanding enginners and/or for raiding enemy engineers.', vid = '', bank = '', cue = '', faction = 'UEF'},
-	{text = '[speed2]: After that you can build additional engineers and tanks. On a small map like Theta you should not build only engineers from your fist factory since you will never need so many of them.', vid = '', bank = '', cue = '', faction = 'UEF'},
-	{text = '[speed2]: Changing factory rally point will also affect which way the units will leave the factory, you can save some time by that.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Factory will build 3 engineers. One for building your base, 2 for expanding. Then 2 tanks and 2 scouts to protect your expanding enginners and/or for raiding enemy engineers.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: After that you can build additional engineers and tanks. On a small map like Theta you should not build only engineers from your fist factory since you will never need so many of them.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Changing factory rally point will also affect which way the units will leave the factory, you can save some time by that.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 FirstEngineer = {
-	{text = '[speed2]: First engineer will go to build a Hydrocarbon and then several Land Factories around the Mass Extractor. The ACU will assist it until the Hydrocarbon and at least one more factory are done.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: First engineer will go to build a Hydrocarbon and then several Land Factories around the Mass Extractor. The ACU will assist it until the Hydrocarbon and at least one more factory are done.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 NorthEngineer = {
-	{text = '[speed2]: Another engineers can expand to the top-left side. It will get few rocks on it\'s way and build Mass Extractors. Try to get the corner area, build a Point Defense and Walls there.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Another engineers can expand to the top-left side. It will get few rocks on it\'s way and build Mass Extractors. Try to get the corner area, build a Point Defense and Walls there.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 SouthEngineer = {
-	{text = '[speed2]: Third engineer will expand to the bottom. Try to secure the area with a Point Defense to deal with enemy attack more easily.', vid = '', bank = '', cue = '', faction = 'UEF'},
-	{text = '[speed2]: This engineer also have an important task of building a T1 Radar next to the hill to give you intel on enemy unit movement.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Third engineer will expand to the bottom. Try to secure the area with a Point Defense to deal with enemy attack more easily.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: This engineer also have an important task of building a T1 Radar next to the hill to give you intel on enemy unit movement.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 SecondFactory = {
-	{text = '[speed2]: Your second Land Factory will produce only attacking units. For the beginning you will needs only tanks and scout. Good ratio is 5 tanks to a single scout. Later you can add an artillery into the mix to deal with enemy Point Defenses', vid = '', bank = '', cue = '', faction = 'UEF'},
-	{text = '[speed2]: Once you build more factories, set them to assist this one. That way you need to set a build qeue only on the one factory which will save you more time to micro your units.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Your second Land Factory will produce only attacking units. For the beginning you will needs only tanks and scout. Good ratio is 5 tanks to a single scout. Later you can add an artillery into the mix to deal with enemy Point Defenses', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Once you build more factories, set them to assist this one. That way you need to set a build qeue only on the one factory which will save you more time to micro your units.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
 
 CommanderMiddle = {
-	{text = '[speed2]: This is also a time to move your ACU to the middle of the map to get the reclaim from there. Build also a Mass Extractor that is on the way.', vid = '', bank = '', cue = '', faction = 'UEF'},
-	{text = '[speed2]: Newly build engineer will replace your ACU with building the base.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: This is also a time to move your ACU to the middle of the map to get the reclaim from there. Build also a Mass Extractor that is on the way.', vid = '', bank = '', cue = '', faction = 'UEF'},
+    {text = '[speed2]: Newly build engineer will replace your ACU with building the base.', vid = '', bank = '', cue = '', faction = 'UEF'},
 }
