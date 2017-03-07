84ea99e6a39b0acd338d3330e4e703a7d639b6f3
speed2CZ <GTGTGT@seznam.cz>
2017-03-07 18:14:09 +0100

    Introduce localization

--- lua/TutorialStrings.lua
+++ lua/TutorialStrings.lua
@@ -1,16 +1,16 @@
 IntroChoice_Dialogue = {
-    {text = '[Training Officer]: Welcome commander, choose if you want to hear the basic information about the map, watch an example build order or try the build order yourself.', vid = 'FAF_TUT_Common_IntroChoice_Dialogue.sfd', bank = 'FAF_TUT_Common_VO', cue = 'IntroChoice_Dialogue', faction = 'UEF'},
+    {text = '<LOC FAF_TUT_Common_IntroChoice_Dialogue>[{i TrainingOfficer}]: Welcome commander, choose if you want to hear the basic information about the map, watch an example build order or try the build order yourself.', vid = 'FAF_TUT_Common_IntroChoice_Dialogue.sfd', bank = 'FAF_TUT_Common_VO', cue = 'IntroChoice_Dialogue', faction = 'UEF'},
 }
-IntroChoice_Title = 'What do you want to do?'
-IntroChoice_MapInfo = 'View basic map info'
-IntroChoice_ExampleBO = 'Watch the build order'
-IntroChoice_TryBO = 'Try the build order'
+IntroChoice_Title = '<LOC FAF_TUT_Common_IntroChoice_Title>What do you want to do?'
+IntroChoice_MapInfo = '<LOC FAF_TUT_Common_IntroChoice_MapInfo>View basic map info'
+IntroChoice_ExampleBO = '<LOC FAF_TUT_Common_IntroChoice_ExampleBO>Watch the build order'
+IntroChoice_TryBO = '<LOC FAF_TUT_Common_IntroChoice_TryBO>Try the build order'
 
 StartBuildOrder = {
-    {text = '[Training Officer]: Let\'s watch an example build order now.', vid = 'FAF_TUT_Common_StartBuildOrder_1.sfd', bank = 'FAF_TUT_Common_VO', cue = 'StartBuildOrder_1', faction = 'UEF'},
-    {text = '[Training Officer]: Hold down your \'SHIFT\' key to see the command queue of the units. You can also select the ACU, Engineers or Factories to see their build queue. In case you need a bit more time to follow the build order, you can adjust the speed with the \'+\' and \'-\' keys, or Pause the game with the \'II\'-Button on the top middle of the screen.', vid = 'FAF_TUT_Common_StartBuildOrder_2.sfd', bank = 'FAF_TUT_Common_VO', cue = 'StartBuildOrder_2', faction = 'UEF'},
+    {text = '<LOC FAF_TUT_Common_StartBuildOrder_1>[{i TrainingOfficer}]: Let\'s watch an example build order now.', vid = 'FAF_TUT_Common_StartBuildOrder_1.sfd', bank = 'FAF_TUT_Common_VO', cue = 'StartBuildOrder_1', faction = 'UEF'},
+    {text = '<LOC FAF_TUT_Common_StartBuildOrder_2>[{i TrainingOfficer}]: Hold down your \'SHIFT\' key to see the command queue of the units. You can also select the ACU, Engineers or Factories to see their build queue. In case you need a bit more time to follow the build order, you can adjust the speed with the \'+\' and \'-\' keys, or Pause the game with the \'II\'-Button on the top middle of the screen.', vid = 'FAF_TUT_Common_StartBuildOrder_2.sfd', bank = 'FAF_TUT_Common_VO', cue = 'StartBuildOrder_2', faction = 'UEF'},
 }
 
 EndBuildOrder = {
-    {text = '[Training Officer]: Now you should know the basics about this map. It\'s time for you to try the build order yourself.', vid = 'FAF_TUT_Common_EndBuildOrder.sfd', bank = 'FAF_TUT_Common_VO', cue = 'EndBuildOrder', faction = 'UEF'},
+    {text = '<LOC FAF_TUT_Common_EndBuildOrder>[{i TrainingOfficer}]: Now you should know the basics about this map. It\'s time for you to try the build order yourself.', vid = 'FAF_TUT_Common_EndBuildOrder.sfd', bank = 'FAF_TUT_Common_VO', cue = 'EndBuildOrder', faction = 'UEF'},
 }
---
+++ mods/tutorials/hook/loc/ES/strings_db.lua
@@ -0,0 +1,12 @@
+TrainingOfficer = "Training Officer"
+
+FAF_TUT_Common_IntroChoice_Dialogue = "[{i TrainingOfficer}]: Welcome commander, choose if you want to hear the basic information about the map, watch an example build order or try the build order yourself."
+FAF_TUT_Common_IntroChoice_Title = "What do you want to do?"
+FAF_TUT_Common_IntroChoice_MapInfo = "View basic map info"
+FAF_TUT_Common_IntroChoice_ExampleBO = "Watch the build order"
+FAF_TUT_Common_IntroChoice_TryBO = "Try the build order"
+
+FAF_TUT_Common_StartBuildOrder_1 = "[{i TrainingOfficer}]: Let's watch an example build order now."
+FAF_TUT_Common_StartBuildOrder_2 = "[{i TrainingOfficer}]: Hold down your 'SHIFT' key to see the command queue of the units. You can also select the ACU, Engineers or Factories to see their build queue. In case you need a bit more time to follow the build order, you can adjust the speed with the '+' and '-' keys, or Pause the game with the 'II'-Button on the top middle of the screen."
+
+FAF_TUT_Common_EndBuildOrder = "[{i TrainingOfficer}]: Now you should know the basics about this map. It's time for you to try the build order yourself."
