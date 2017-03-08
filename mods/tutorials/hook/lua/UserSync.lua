bfa82a7d73d9354f069f33a14995369d8812e6e2
TheKeyBlue <keyblue357@hotmail.com>
2017-03-08 20:05:22 +0100

    Allow +-* shorcuts for gamespeed change during NoOrdersAllowed

--- mods/tutorials/hook/lua/UserSync.lua
+++ mods/tutorials/hook/lua/UserSync.lua
@@ -1,5 +1,15 @@
 NoOrdersAllowed = false
 
+local allowedKeyMap =
+{
+    ['NumMinus']    = {action =  'UI_Lua import("/lua/ui/uimain.lua").DecreaseGameSpeed()',
+        category = 'game', order = 1,},
+    ['NumPlus']     = {action =  'UI_Lua import("/lua/ui/uimain.lua").IncreaseGameSpeed()',
+        category = 'game', order = 2,},
+    ['NumStar']     = {action =  'WLD_ResetSimRate',
+        category = 'game', order = 3,},
+}
+
 local oldOnSync = OnSync
 OnSync = function()
     oldOnSync()
@@ -17,7 +27,9 @@ OnSync = function()
     if Sync.NoOrdersAllowed then
         NoOrdersAllowed = Sync.NoOrdersAllowed
         IN_ClearKeyMap()
+        IN_AddKeyMapTable(allowedKeyMap)
     elseif Sync.NoOrdersAllowed == false then
+        IN_ClearKeyMap()
         NoOrdersAllowed = Sync.NoOrdersAllowed
         IN_AddKeyMapTable(import('/lua/ui/game/gamemain.lua').modifiersKeys)
         import('/modules/hotbuild.lua').addModifiers()
